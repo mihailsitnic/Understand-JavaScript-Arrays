00f22a4c8b9a9ddcd96eb33130003c6d2d86d1d6
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-17 17:38:45 +0200

    Array.prototype.slice();

---
+++ scripts/slice.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* Array.prototype.slice();
+/* ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## */
+
+var itemsSlice = [1, 2, 3, 4, 5];
+var copy = itemsSlice.slice(0, 1);
+copy.push(6);
+copy[0] = 100;
+console.log(itemsSlice);
+console.log(copy);
+
+// example
+var pesonSlice = {
+  name: 'mihail-sitnic'
+};
+var filters = {
+  'deslugify': x => x.replace('-', ' '),
+  'uppercase': x => x.toUpperCase()
+};
+var input = 'name | deslugify | uppercase'; // SHANE OSBOURNE
+var section = input.split('|').map(x => x.trim()); // [name, deslugify, uppercase]
+var ref = pesonSlice[section[0]];
+var output = section
+  .slice(1)
+  .reduce((prev, next) => {
+    if(filters[next]){
+      return filters[next].call(null, prev);
+    }
+    return prev
+  }, ref);
+
+console.log(output);
