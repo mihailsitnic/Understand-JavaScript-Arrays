577f9223c32924fa94c067b0ecf2e4a2b1670f0e
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-17 18:02:18 +0200

    Array.prototype.push();

---
+++ scripts/push.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Array.prototype.push();
+/* Метод push() добавляет один или более элементов в конец массива 
+/* и возвращает новую длину массива.
+/* ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## */
+
+const petsPush = ['cat', 'dog'];
+const wishlistPush = ['hamster', 'horse', 'snake'];
+petsPush.push.apply(petsPush, wishlistPush);
+console.log(petsPush)
